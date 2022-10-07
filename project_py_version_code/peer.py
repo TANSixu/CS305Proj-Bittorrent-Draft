5d037cfb6ed75c0a12b03621f2f343f2d8482936
MarSxTan <846030067@qq.com>
2022-10-07 00:30:23 -0800

    start py code

---
+++ project_py_version_code/peer.py
@@ -0,0 +1,15 @@
+import pickle
+import socket
+
+HOST = "127.0.0.1"
+PORT = 38324
+
+def process_user_input():
+    pass
+
+def process_inbound_udp(sock):
+    pass
+
+def peer_run(config):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        pass
